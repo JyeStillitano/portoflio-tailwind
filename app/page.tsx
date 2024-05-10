3ad4590fce1036afed1949ff771436e36cfecb6c
Jye J. Stillitano <jyestillitano@hotmail.com>
2024-05-10 16:08:13 +1000

    NavBar icons and styling updated.

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,29 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     // Outer Container
-    <div className="h-full max-w-7xl">
+    <div className="h-full max-w-5xl m-auto">
       {/* Head */}
       <div className="flex justify-center mb-4">
-        <div className="bg-white rounded-b-xl shadow-xl px-8 py-4">
-          <h1 className="text-3xl">Jye Stillitano</h1>
+        <div className="bg-black rounded-b-xl shadow-xl px-8 py-4">
+          <h1 className="text-3xl text-white font-thin tracking-wider">
+            Jye Stillitano
+          </h1>
         </div>
       </div>
 
       {/* Body */}
-      <div className="flex flex-row">
+      <div className="flex">
         {/* NavBar Container */}
         <div className="m-4">
           <NavBar />
         </div>
+
         {/* Content Container */}
-        <div className="bg-white rounded-xl shadow-lg m-4 flex-grow p-4"></div>
+        <div className="bg-white flex-grow rounded-xl shadow-lg m-4 p-4 content-center align-middle">
+          <div className="flex justify-center">
+            <p className="text-xl text-gray-400">Work in progress...</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,13 +1,91 @@
 import Image from "next/image";
 
+// Icons credit: https://heroicons.com
+
+var selection: number = 1;
+
 export default function NavBar() {
   return (
-    <div className="flex flex-col min-w-12 max-w-24 h-96 justify-evenly rounded-xl shadow-lg bg-white">
-      <i className="flex justify-center">A</i>
-      <i className="flex justify-center">B</i>
-      <i className="flex justify-center">C</i>
-      <i className="flex justify-center">D</i>
-      <i className="flex justify-center">E</i>
+    <div className="flex flex-col w-24 h-96 justify-evenly rounded-xl shadow-lg bg-white">
+      {/* Profile Icon */}
+      <div className="flex justify-center">
+        <img
+          className=" rounded-full w-12 h-12 border-2 border-black"
+          src="./Profile.jpg"
+        />
+      </div>
+
+      {/* Mail Icon */}
+      <div className="flex justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+          />
+        </svg>
+      </div>
+
+      {/* Code Icon */}
+      <div className="flex justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+          />
+        </svg>
+      </div>
+
+      {/* Art Icon */}
+      <div className="flex justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+          />
+        </svg>
+      </div>
+
+      {/* Music Icon */}
+      <div className="flex justify-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
