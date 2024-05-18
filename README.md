de5cc02c5386be94a040e9b1986ec1ee08f709c2
Jye J. Stillitano <jyestillitano@hotmail.com>
2024-05-18 17:40:39 +1000

    Updated README. Updated main page content.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Next.js](https://nextjs.org/) web app. You can follow my design process on [Figma](https://www.figma.com/design/7CQBA0EwAEx7YvXiFRIskO/Website?node-id=0%3A1&t=2exI8fWz6AsemVWX-1).
 
-## Getting Started
+# What's The Tech Stack?
 
-First, run the development server:
+This project has been bootstrapped using [create-next-app](https://github.com/vercel/next.js/tree/canary/packages/create-next-app). In such, the Next framework is at the core of this technical stack. The primary functionality has been written in [TypeScript](https://www.typescriptlang.org/), Microsoft's statically typed extension of JavaScript. This has been chosen to catch errors in code early on and for the ease of development that comes from modern IDE's autocompletion.
+
+## Running the development environment.
+
+To run the development server, navigate to the project directory and execute the following command:
 
 ```bash
 npm run dev
@@ -14,23 +18,10 @@ pnpm dev
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Once the development server is running, navigate to [http://localhost:3000](http://localhost:3000) in your browser of choice.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Deploying to production.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+This project is currently hosted on [GitHub](https://github.com/JyeStillitano/jyestillitano.github.io).
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+The project has been setup with a Next.js YAML template to, on push to main, automatically build and deploy to a free GitHub Pages website, currently using the default domain [https://jyestillitano.github.io/](https://jyestillitano.github.io/).
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 import Image from "next/image";
+import programming_image from "@/public/programming.svg";
+import programming_guy_image from "@/public/programming-guy.svg";
+import reactjs_icon from "@/public/reactjs.svg";
+import nextjs_icon from "@/public/nextjs.svg";
+import nodejs_icon from "@/public/nodejs.svg";
+import tailwind_icon from "@/public/tailwind.svg";
+import figma_icon from "@/public/figma.svg";
+
 import NavBar from "@/components/NavBar";
 
 export default function Home() {
@@ -15,17 +23,75 @@ export default function Home() {
       </div>
 
       {/* Body */}
-      <div className="flex">
-        {/* NavBar Container */}
-        <div className="m-4">
-          <NavBar />
-        </div>
 
-        {/* Content Container */}
-        <div className="bg-white flex-grow rounded-xl shadow-lg m-4 p-4 content-center align-middle">
-          <div className="flex justify-center">
-            <p className="text-xl text-gray-400">Work in progress...</p>
+      {/* NavBar Container */}
+      <div className="fixed top-1/4">
+        <NavBar />
+      </div>
+
+      {/* Content Container */}
+      <div className="bg-white flex-grow rounded-xl shadow-lg mx-36 p-4">
+        {/* Content */}
+        <div className="flex-col">
+          <h1>Who Am I?</h1>
+          <p>
+            Hi there, I'm Jye, a Melbourne based programmer with a Bachelors of
+            Computer Science, majoring in Software Development.
+          </p>
+          <Image src={programming_image} alt="Programming illustration." />
+          <p>
+            I have created this website as a hobby project and as a place to
+            showcase my personal work. <br />
+            It has been built using the following tools and frameworks...
+          </p>
+          <div className="flex flex-row justify-evenly">
+            <Image
+              className="mx-4"
+              src={reactjs_icon}
+              alt="React JS"
+              width="100"
+              height="100"
+            />
+            <Image
+              className="mx-4"
+              src={nextjs_icon}
+              alt="Next JS"
+              width="100"
+              height="100"
+            />
+            <Image
+              className="mx-4"
+              src={nodejs_icon}
+              alt="Node JS"
+              width="100"
+              height="100"
+            />
+            <Image
+              className="mx-4"
+              src={tailwind_icon}
+              alt="Tailwind CSS"
+              width="100"
+              height="100"
+            />
+            <Image
+              className="mx-4"
+              src={figma_icon}
+              alt="Figma"
+              width="100"
+              height="100"
+            />
           </div>
+          <p>
+            The project is completely open source and available on my{" "}
+            <a href="https://github.com/JyeStillitano/jyestillitano.github.io">
+              GitHub
+            </a>
+            .
+          </p>
+          <Image
+            src={programming_guy_image}
+            alt="Programming guy illustration."
+          />
         </div>
       </div>
     </div>
