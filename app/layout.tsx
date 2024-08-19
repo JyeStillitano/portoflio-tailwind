99730e348248dd3b1955623df5c83952404007fe
Jye J. Stillitano <jyestillitano@hotmail.com>
2024-08-19 19:53:49 +1000

    Added a redirect to new dynamic app deployment.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-import type { Metadata } from "next";
+"use client";
+
+import { useEffect } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +11,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    const currentHost = window.location.hostname;
+
+    // Check if the user is on jyestillitano.github.io
+    if (currentHost === "jyestillitano.github.io") {
+      // Redirect to jyes.dev
+      window.location.href = "https://jyes.dev";
+    }
+  });
+
   return (
     <html lang="en" className="">
       <body className="m-0 h-full bg-background p-0">{children}</body>
