d7e9391f1c70b6380dbe7a4d6c845262d0b1bed4
Jye J. Stillitano <jyestillitano@hotmail.com>
2024-09-01 15:03:28 +1000

    Styling updates (favicon, css).

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="mb-4 flex justify-center">
       <div className="sticky top-0 rounded-b-xl border-[2px] border-t-0 border-accent bg-primary px-8 py-4 shadow-lg">
-        <h1 className="text-bg text-3xl font-thin tracking-wider">
+        <h1 className="text-3xl font-thin tracking-wider text-white">
           Jye Stillitano
         </h1>
       </div>
